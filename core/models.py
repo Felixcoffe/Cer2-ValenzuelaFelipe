d934ccf50a136120551fde612dd27778dc9c9d16
Felixcoffe <Felipe.valenzuelahu@usm.cl>
2022-11-13 23:47:26 -0300

    no lo logre hacer la pagina web con bd

--- core/models.py
+++ core/models.py
@@ -4,19 +4,27 @@ from django.db import models
 
 class Residencia(models.Model):
     Num_residencia=models.IntegerField()
-    Propietario=models.CharField(max_length=30)
+    Propietario=models.CharField(max_length=30, default="Hoteleria")
     Estado_viv= (
         ('Ocupado','Ocupado'),
         ('Disponible','Disponible'),
         ('En Mantenimiento','En mantenimiento')
         )
-    Estado = models.CharField(max_length=30, choices= Estado_viv)
+    Estado = models.CharField(max_length=30, choices=Estado_viv)
+    def __str__(self):
+        return 'vivienda %s Estado: %s' % (self.Num_residencia, self.Estado)
+    
 
 class Correspondencia(models.Model):
     fecha_llegada= models.DateField()
-    Destino= models.IntegerField(null=True)
+    nombre= models.CharField(max_length=30, null=True)
+    Destino= models.CharField(max_length=4 ,null=True)
     Estado_Cor= (
         ('En Consejeria','En Consejeria'),
         ('Entregada','Entregada')
     ) 
     Estado = models.CharField(max_length=30, choices=Estado_Cor)
+    fecha_entregado= models.DateField(null=True, blank=True)
+    def __str__(self):
+        return '%s %s %s' % (self.nombre, self.Destino, self.Estado)
+    
--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Correspondencia
+from django.db.models import Q
 
 # Create your views here.
+
+def home(request):
+    return render(request,'core/home.html')
+
+def paquetes(request):
+    return render(request, 'core/paquetes.html')
+
+def buscar(request):
+    if request.GET["correo"]:
+        mensaje= "Correo Buscado %r" %request.GET["correo"]
+        correo1= request.GET["correo"]
+    
+        x= Correspondencia.objects.filter(Destino__icontains=correo1)
+        return render(request,"core/home.html",{"Correspondencias":x, "query":correo1})
+    else: 
+        mensaje = "Error"    
+    return HttpResponse(mensaje)
